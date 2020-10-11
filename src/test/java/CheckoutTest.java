53d136a26d285b2a034df676d77b4b5fee2edc7e
lasiqueira <lucas.asiq@gmail.com>
2020-10-11 23:33:08 +0100

    initial commit

---
+++ src/test/java/CheckoutTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CheckoutTest {
+
+    @BeforeEach
+    public void setup(){
+
+    }
+
+    @Test
+    @DisplayName("Test adding item to basket")
+    public void addItemTest(){
+
+    }
+
+    @Test
+    @DisplayName("Test getting total without discount")
+    public void getTotalWithoutDiscount(){
+
+    }
+
+    @Test
+    @DisplayName("Test getting total without discount")
+    public void getTotalWithoutDiscount(){
+
+    }
+
+    @Test
+    @DisplayName("Test getting total with 10% discount after £60")
+    public void getTotalWith10PercentOver60Discount(){
+
+    }
+
+    @Test
+    @DisplayName("Test getting total with £8.50 discount after 2+ products")
+    public void getTotalWith850After2PlusDiscount(){
+
+    }
+
+
+
+}
